32dbe0d5a22e652b95366917b4d3aa835bf4a962
Mbicha <mbithicharlse@gmail.com>
2022-03-28 01:30:22 -0700

    Added a function that checks if a character is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_islower - The function to check lower case letter
+*@c: input character
+*
+*Return: 1 if lower 0 if upper
+*/
+
+int _islower(int c)
+{
+char checklower;
+int lower = 0;
+
+for (checklower = 'a'; checklower <= 'z'; checklower++)
+{
+if (checklower == c)
+{ 
+lower = 1;
+}
+}
+return (lower);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,4 @@ This directory contains functions and nested loops in c
 Task 0: Prints _putchar
 Task 1: Prints lower case alphabets.
 Task 2: Prints alphabets 10 times followed by new line.
+Task 3: Check if leter is lower.
