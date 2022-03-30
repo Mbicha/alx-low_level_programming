f01c525393f460a416d6b1d297feaef587baace8
Mbicha <mbithicharlse@gmail.com>
2022-03-30 09:31:25 -0700

    Print factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*factorial - Function factorial of a number
+*@n: input integer
+*Return: Factorial
+*/
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+
+if (n <= 1)
+{
+return (1);
+}
+
+return (n * factorial(n-1));
+
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,5 @@ Task 0: Prints string using recursion.
 Task 1: Function that prints a string in reverse.
 
 Task 2: Function to return length of a string.
+
+Task 3: Function to print factorial of a number
