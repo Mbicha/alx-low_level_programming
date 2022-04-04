d98a2746e137eb33e16ffc1550dbba03899cbfd7
Mbicha <mbithicharlse@gmail.com>
2022-04-03 22:54:04 -0700

    Function that multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*mul - Function that multiplies 2 integers
+*@a: Input integer
+*@b: Input integer
+*Return: Multiplication of 2 integers
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Task 0: checks if a character is upper
 Task 1: A fuction that checks for a digit (0 - 9)
+Task 2: A function that multiplies 2 integers
