2ffceb504ef60c46cc94f588c71ae38b6bcb3a0f
Mbicha <mbithicharlse@gmail.com>
2022-04-04 23:55:08 -0700

    A program that prints all arguments passed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,13 +9,18 @@
 */
 int main(int argc, char *argv[])
 {
-int count;
 
-for (count = 1; count < argc; count++)
+while (argc--)
 {
-printf("%s\n", argv[count]);
+printf("%s\n", *argv++);
 }
+/*int count;
 
+*for (count = 1; count < argc; count++)
+*{
+*printf("%s\n", argv[count]);
+*}
+*/
 return (0);
 
 }
