f7a1292ba1d1ac4c8dc584f21b94c55bb73c2223
Mbicha <mbithicharlse@gmail.com>
2022-04-15 10:46:34 -0700

    Function to print strings

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*print_all - A function prints anyting.
+*@format: A list of type of arguments passed to the function.
+*Return: Nothing
+*/
+void print_all(const char * const format, ...)
+{
+
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,3 +3,5 @@ Task 0: Function to sum all variables.
 Task 1: A function that print numbers followed by a new line.
 
 Task 2: A function to print strings.
+
+Task 3: A function that prints anything.
