6cdc407205ff113f199c4e917811158741ae1af1
Mbicha <mbithicharlse@gmail.com>
2022-04-22 05:13:22 -0700

    Function to return list of elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+*print_list - Function that prints all elements
+*@h: pointer t head of list
+*Return: number of nodes in a list
+*/
+size_t print_list(const list_t *h)
+{
+    unsigned int nodes = 0;
+
+    while (h)
+    {
+        printf("[%u] ", h->len);
+        if (h->str == NULL)
+            printf("(nill)\n");
+        else
+            printf("%s\n", h->str);
+
+        h = h->next;
+        nodes++;
+    }
+    
+    return nodes;
+}
