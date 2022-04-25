1b0c8696e689298286264bd0209b78e0d6c4ac2d
Mbicha <mbithicharlse@gmail.com>
2022-04-25 05:45:32 -0700

    Free list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,12 +7,6 @@
 *@str: value of created node
 *Return: address of the created element or NULL if failed
 */
-/*typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;*/
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
