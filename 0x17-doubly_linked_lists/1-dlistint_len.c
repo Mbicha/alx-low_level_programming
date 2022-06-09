7cf83443774da38b0d480d075696fe013350e74d
Mbicha <mbithicharlse@gmail.com>
2022-06-09 22:55:55 +0300

    Count number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function to count number of elements
+ * @h: Linked list head
+ *
+ * Return: Number of nodes (size_t)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t count = 0;
+    while (h != Null)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
+}
